f69c4de03af4d9962165951636057b827608e0be
John Ma <jiama843@gmail.com>
2019-11-24 13:09:26 -0500

    split soda.cc

---
+++ group.h
@@ -0,0 +1,11 @@
+#ifndef GROUP_H
+#define GROUP_H
+
+_Task Groupoff {
+	void main();
+  public:
+	Groupoff( Printer & prt, unsigned int numStudents, unsigned int sodaCost, unsigned int groupoffDelay );
+	WATCard::FWATCard giftCard();
+};
+
+#endif
---
+++ vendingmachine.h
@@ -0,0 +1,19 @@
+#ifndef VENDINGMACHINE_H
+#define VENDINGMACHINE_H
+
+_Task VendingMachine {
+	void main();
+  public:
+	enum Flavours { ... }; 				// flavours of soda (YOU DEFINE)
+	_Event Free {};						// free, advertisement
+	_Event Funds {};					// insufficient funds
+	_Event Stock {};					// out of stock for particular flavour
+	VendingMachine( Printer & prt, NameServer & nameServer, unsigned int id, unsigned int sodaCost );
+	void buy( Flavours flavour, WATCard & card );
+	unsigned int * inventory();
+	void restocked();
+	_Nomutex unsigned int cost() const;
+	_Nomutex unsigned int getId() const;
+};
+
+#endif
