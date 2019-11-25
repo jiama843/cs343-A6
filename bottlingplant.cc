c4df7616bea73f9fb4f0b7563b6361d7b99fccaf
Leo Yoon <bcyoon@uwaterloo.ca>
2019-11-24 21:14:07 -0500

    Finished BottlingPlant

--- bottlingplant.cc
+++ bottlingplant.cc
@@ -1,4 +1,7 @@
 #include "bottlingplant.h"
+#include "MPRNG.h"  // the random number generator
+
+extern MPRNG mprng;  // global random number generator
 
 // TODO:
 /*
@@ -21,15 +24,74 @@
 
 */
 
-BottlingPlant::BottlingPlant( Printer & prt, NameServer & nameServer, unsigned int numVendingMachines,
-unsigned int maxShippedPerFlavour, unsigned int maxStockPerFlavour,
-unsigned int timeBetweenShipments )
-: {}
+// ---------------------------------
+//  Private Methods
+// ---------------------------------
+void BottlingPlant::main() {
+    // begin by creating a truck
+    Truck truck( prt, nameServer, *this, numVendingMachines, maxStockPerFlavour );
 
-void BottlingPlant::main(){
+    for ( ;; ) {
+        try {
+            /*
+             * periodically produce random new quantities of each flavour of soda,
+             * [0, MaxShippedPerFlavour] per flavour.
+             */
+            inventory[0] = mprng( maxShippedPerFlavour );
+            inventory[1] = mprng( maxShippedPerFlavour );
+            inventory[2] = mprng( maxShippedPerFlavour );
+            inventory[3] = mprng( maxShippedPerFlavour );
 
-}
+            /*
+             * To simulate a production run of soda, the bottling plant
+             * yields for TimeBetweenShipments times (not random).
+             */
+            yield( timeBetweenShipments );
 
-void BottlingPlant::getShipment( unsigned int cargo[] ){
+            _Accept( ~BottlingPlant ) {
+                shouldThrowShutdown = true;
+                break;
+            }
+            or _Accept( getShipment ) {
+                // TODO: Do we need to do anything here?
+            }  // _Accept
+        } catch ( uMutexFailure::RendezvousFailure& ) {
+            // so we know there is a Shutdown here
+        }  // try
 
-}
+    }  // for
+
+}  // BottlingPlant::main
+
+// ---------------------------------
+//  Constructor
+// ---------------------------------
+BottlingPlant::BottlingPlant( Printer& prt, NameServer& nameServer, unsigned int numVendingMachines,
+                              unsigned int maxShippedPerFlavour, unsigned int maxStockPerFlavour,
+                              unsigned int timeBetweenShipments ) : prt( prt ), nameServer( nameServer ), numVendingMachines( numVendingMachines ), maxShippedPerFlavour( maxShippedPerFlavour ), maxStockPerFlavour( maxShippedPerFlavour ), timeBetweenShipments( timeBetweenShipments ), inventory( { 0, 0, 0, 0 } ), shouldThrowShutdown( false ) {
+}  // BottlingPlant::BottlingPlant
+
+// ---------------------------------
+//  Public Methods
+// ---------------------------------
+
+// getShipment throws the exception Shutdown if the bottling plant is closing down
+void BottlingPlant::getShipment( unsigned int cargo[] ) {
+    /*
+     * getShipment throws the exception Shutdown if the bottling plant is closing
+     * down and no shipment is copied into the cargo array passed by the truck
+     */
+    if ( shouldThrowShutdown ) {
+        _Throw Shutdown();
+    }  // if
+
+    /*
+     * The truck calls getShipment to obtain a shipment from the plant 
+     * (i.e., the production run), and the shipment is copied into the
+     * cargo array passed by the truck
+     */
+    cargo[0] = inventory[0];
+    cargo[1] = inventory[1];
+    cargo[2] = inventory[2];
+    cargo[3] = inventory[3];
+}  // BottlingPlant::getShipment
--- bottlingplant.h
+++ bottlingplant.h
@@ -1,14 +1,29 @@
 #ifndef BOTTLING_H
 #define BOTTLING_H
 
+#include "nameserver.h"
+#include "printer.h"
+
 _Task BottlingPlant {
-	void main();
-  public:
-	_Event Shutdown {};					// shutdown plant
-	BottlingPlant( Printer & prt, NameServer & nameServer, unsigned int numVendingMachines,
-				 unsigned int maxShippedPerFlavour, unsigned int maxStockPerFlavour,
-				 unsigned int timeBetweenShipments );
-	void getShipment( unsigned int cargo[] );
+    Printer&     prt;
+    NameServer&  nameServer;
+    unsigned int numVendingMachines;
+    unsigned int maxShippedPerFlavour;
+    unsigned int maxStockPerFlavour;
+    unsigned int timeBetweenShipments;
+
+    unsigned int inventory[4];         // there are 4 different flavours
+    bool         shouldThrowShutdown;  // set to true once Destructor is called
+
+    void main();
+
+   public:
+    _Event Shutdown{};  // shutdown plant
+
+    BottlingPlant( Printer & prt, NameServer & nameServer, unsigned int numVendingMachines,
+                   unsigned int maxShippedPerFlavour, unsigned int maxStockPerFlavour,
+                   unsigned int timeBetweenShipments );
+    void getShipment( unsigned int cargo[] );
 };
 
 #endif
