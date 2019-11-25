914daf03ffc5b248e9b824d07a595737e078ce3d
Leo Yoon <bcyoon@uwaterloo.ca>
2019-11-24 22:04:27 -0500

    Finished Bank

--- bank.h
+++ bank.h
@@ -3,14 +3,15 @@
 
 // A monitor that behaves like a server
 _Monitor Bank {
-    unsigned int numStudents;
+    unsigned int  numStudents;  // number of students in the system
+    unsigned int *accounts;     // data structure to store students' bank information
 
-	// TODO: Need a data structure to store students' bank information
+   public:
+    Bank( unsigned int numStudents );
+    ~Bank();
 
-   public: 
-	Bank( unsigned int numStudents );
     void deposit( unsigned int id, unsigned int amount );
-	void withdraw( unsigned int id, unsigned int amount );
+    void withdraw( unsigned int id, unsigned int amount );
 };
 
 #endif
