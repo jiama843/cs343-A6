43b9091ea91de560bb6525652993709dbfa51a91
John Ma <jiama843@gmail.com>
2019-11-26 16:14:10 -0500

    vending machine

--- truck.cc
+++ truck.cc
@@ -32,7 +32,7 @@ void Truck::restock_vending(VendingMachine ** vlist){
     lastVendingMachine++;
     lastVendingMachine %= numVendingMachines;
 
-    printer.print(Printer::Kind::Truck, 'd', lastVendingMachine, cargo[0] + cargo[1] + cargo[2] + cargo[3]);
+    prt.print(Printer::Kind::Truck, 'd', lastVendingMachine, cargo[0] + cargo[1] + cargo[2] + cargo[3]);
     int *inv = vlist[lastVendingMachine]->inventory();
 
     for(int j = 0; j < NUM_FLAVOURS; j++){
@@ -40,25 +40,23 @@ void Truck::restock_vending(VendingMachine ** vlist){
       // Fill with as much soda as possible
       for(;;){
         if (inv[j] >= maxStockPerFlavour) break;
-        if (cargo[j] <= 0) {
-          int b_remain = maxStockPerFlavour * 4 - (inv[0] + inv[1] + inv[2] + inv[3]);
-          if(b_remain > 0) printer.print(Printer::Kind::Truck, 'U', lastVendingMachine, b_remain);
-          break;
-        }
+        if (cargo[j] <= 0) break;
 
         inv[j]++;
         cargo[j]--;
       }
     }
+    int b_remain = maxStockPerFlavour * 4 - (inv[0] + inv[1] + inv[2] + inv[3]);
+    if(b_remain > 0) prt.print(Printer::Kind::Truck, 'U', lastVendingMachine, b_remain);
 
-    printer.print(Printer::Kind::Truck, 'D', i, cargo[0] + cargo[1] + cargo[2] + cargo[3]);
+    prt.print(Printer::Kind::Truck, 'D', lastVendingMachine, cargo[0] + cargo[1] + cargo[2] + cargo[3]);
     vlist[lastVendingMachine]->restocked();
   }
 }
 
 
 void Truck::main(){
-  printer.print(Printer::Kind::Truck, 'S');
+  prt.print(Printer::Kind::Truck, 'S');
 
   // Array of pointers to vending machines
   VendingMachine ** vlist = nameServer.getMachineList();
@@ -68,13 +66,13 @@ void Truck::main(){
       yield(mprng(1, 10));
       cargo = {0, 0, 0, 0};
       plant.getShipment(cargo);
-      printer.print(Printer::Kind::Truck, 'P', cargo[0] + cargo[1] + cargo[2] + cargo[3]);
+      prt.print(Printer::Kind::Truck, 'P', cargo[0] + cargo[1] + cargo[2] + cargo[3]);
 
       restock_vending(vlist);
     }
   }
   _Catch(BottlingPlant::Shutdown &){
     // handle Shutdown
-    printer.print(Printer::Kind::Truck, 'F');
+    prt.print(Printer::Kind::Truck, 'F');
   }
 }
