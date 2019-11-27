e8cad08107f06d0b0d237963358612b1561b7b7a
John Ma <jiama843@gmail.com>
2019-11-26 22:19:03 -0500

    added private fields

--- groupoff.h
+++ groupoff.h
@@ -10,6 +10,11 @@
 extern MPRNG mprng;
 
 _Task Groupoff {
+	Printer &prt;
+	unsigned int numStudents;
+	unsigned int sodaCost;
+	unsigned int groupoffDelay;
+
 	vector<WATCard::WATCard*> cardlist; // list of pointers to watcards
 	vector<WATCard::FWATCard> fcards; // list of future watcards
 	void main();
