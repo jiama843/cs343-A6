62acde15b6e072a892ad2eba74248e5631c9c473
Leo Yoon <bcyoon@uwaterloo.ca>
2019-11-26 22:39:00 -0500

    Fixed compile errors for VendingMachine, I think

--- vendingmachine.h
+++ vendingmachine.h
@@ -1,36 +1,43 @@
 #ifndef VENDINGMACHINE_H
 #define VENDINGMACHINE_H
 
+#include "nameserver.h"
 #include "printer.h"
 #include "watcard.h"
-#include "nameserver.h"
 
 _Task VendingMachine {
-	int id;
-	int sodaCost;
-	Printer &prt;
-	NameServer &nameServer;
+    Printer &    prt;
+    NameServer & nameServer;
+    unsigned int id;
+    unsigned int sodaCost;
 
-	int * stock[4] = {0};
+    unsigned int stock[4] = { 0 };
 
-	void main();
+    void main();
 
-public:
-	enum Flavours { A, B, C, D }; 				// flavours of soda (YOU DEFINE)
-	_Event Free {};						// free, advertisement
-	_Event Funds {};					// insufficient funds
-	_Event Stock {};					// out of stock for particular flavour
-	VendingMachine( Printer & prt, NameServer & nameServer, unsigned int id, unsigned int sodaCost );
-	void buy( Flavours flavour, WATCard & card );
-	unsigned int * inventory();
-	void restocked();
-	_Nomutex unsigned int cost() const;
-	_Nomutex unsigned int getId() const;
+   public:
+    enum Flavours { A,
+                    B,
+                    C,
+                    D };  // flavours of soda (YOU DEFINE)
 
-private:
-	// Local variables
-	WATCard card;
-	Flavours flavour;
+    _Event Free{};   // free, advertisement
+    _Event Funds{};  // insufficient funds
+    _Event Stock{};  // out of stock for particular flavour
+
+    VendingMachine( Printer & prt, NameServer & nameServer, unsigned int id, unsigned int sodaCost );
+
+    void          buy( Flavours flavour, WATCard & card );
+    unsigned int *inventory();
+    void          restocked();
+
+    _Nomutex unsigned int cost() const;
+    _Nomutex unsigned int getId() const;
+
+   private:
+    // local variables
+    WATCard *card;
+    Flavours flavour;
 };
 
 #endif
